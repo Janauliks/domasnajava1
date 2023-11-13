cd1dcec15301d4549987a6678348d0d789a062b9
Janauliks <146983739+Janauliks@users.noreply.github.com>
2023-11-13 19:52:52 +0100

    Add files via upload

---
+++ domasnajava1.js
@@ -0,0 +1,42 @@
+let hasCoffee = true
+let hasTea = true
+let hasMilk = true
+console.log(hasTea || hasMilk)
+console.log(hasCoffee && hasTea) 
+console.log(!hasTea)
+console.log(hasCoffee && hasMilk) || (!hasCoffee && hasMilk)
+console.log(hasTea && hasMilk) || (hasCoffee && hasTea)
+console.log(!hasCoffee && !hasTea)
+
+
+const num1 = 10;
+const num2 = 5;
+const num3 = 8;
+const num4 = 2;
+const num5 = 12;
+const num6 = 4;
+const num7 = 3;
+const result1 = (num1 - num2) + (num3 - num4)
+console.log(result1)
+const result2 = (num3 - num4) - (num5 / num6)
+console.log(result2)
+const result3 = (num5 / num4) - (num7 / (num3 * num6))
+console.log(result3)
+
+
+let num10 = 12
+let num20 = 4
+
+sum1 = (num10 + num20)
+console.log(sum1)
+
+diff1 = (num10 - num20)
+console.log(diff1)
+
+mult1 = (num10 * num20)
+console.log(mult1)
+
+
+div1 = (num10 / num20)
+console.log(div1)
+
